eb631cab82066af25a4a364d22ace73f93fb8652
Henrique Miranda <enrque.ns@gmail.com>
2020-09-15 23:21:54 -0300

    refactor: remove unnecessary folder

--- packages/chat-block/react/Chat.tsx
+++ packages/chat-block/react/Chat.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
+import { useCssHandles } from 'vtex.css-handles'
 
 import { ReactComponent as BotImg } from '../assets/bot.svg';
 import './styles.css';
 
 interface ChatProps {}
 
+const CSS_HANDLES = ['floatchat', 'floatBottom'] as const
+
 const Chat: StorefrontFunctionComponent<ChatProps> = () => {
+  const handles = useCssHandles(CSS_HANDLES)
   const [isOpened, setOpened] = useState(false);
 
   function handleOnClick() {
     setOpened(state => !state);
   }
-
+// className="floatchat-container"
+// className={`chat ${isOpened ? 'is-visible' : ''}`}
+// className="float-bottom"
   return (
-    <div className="floatchat-container">
+    <div className={handles.floatchat} style={{
+      position: 'absolute',
+      right: '32px',
+      bottom: '32px',
+      borderColor: '#E76F51',
+      borderWidth: '2px',
+    }}>
       <div className={`chat ${isOpened ? 'is-visible' : ''}`}>
         <iframe src="https://d1ynr93g6z78ed.cloudfront.net/index.html"
-          width="500px"
+          width="400px"
           height="500px" />
       </div>
 
-      <button className="float-bottom" type="button" onClick={handleOnClick}>
+      <button className={handles.floatBottom} type="button" >
         <BotImg />
       </button>
     </div>
